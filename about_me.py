7a4fcd2b81698c6a68eb5fd1c65e4440150d8dd1
ryansheng <ryansheng@proton.me>
2026-05-02 20:48:05 -0700

    Initial Commit

---
+++ about_me.py
@@ -0,0 +1,23 @@
+first_name = "Ryan"
+last_name = "You"
+age = 49
+city = "Arcadia"
+career ="IT Support"
+
+
+print(f"""{'':=^50}\n
+{'':20} Studdent Profile \n
+{'':=^50} \n
+Name:{'':20}{first_name+' '+last_name} \n
+Age:{'':21}{age} \n
+City:{'':20}{city} \n
+Previous Career:{'':9}{career} \n
+
+Why I'm learning to code: \n
+"Because I think coding is a fun challenage." \n
+
+Fun fact: I lodt three teeth in one month.
+
+{'':=^50}
+""")
+
